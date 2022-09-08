f5c900b244d8f0930da19bf983be265fb008c9e9
jplataquin <jp.lataquin@gmail.com>
2022-09-09 01:05:27 +0800

    test

--- app/Http/Controllers/PartnerController.php
+++ app/Http/Controllers/PartnerController.php
@@ -19,7 +19,7 @@ class PartnerController extends Controller
         $brand_id       = (int) $request->input('brand');
         $branch         = $request->input('branch');
         $email          = $request->input('email');
-        $uername        = $request->input('username');
+        $username        = $request->input('username');
         $status         = $request->input('status');
 
         $primary_contact_person             = $request->input('primary_contact_person');
@@ -35,7 +35,7 @@ class PartnerController extends Controller
         $partner->password  = Hash::make('12345678');
         $partner->email     = $email;
         $partner->brand_id  = $brand_id;
-        $partner->status    = '';
+        $partner->status    = $status;
 
         $partner->primary_contact_person            = $primary_contact_person;
         $partner->primary_contact_no                = $primary_contact_no;
