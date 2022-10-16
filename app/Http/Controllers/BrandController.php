0f6ee6a6900f19a755e0aeccbc574c9fddcbc2a3
jplataquin <jp.lataquin@gmail.com>
2022-10-17 03:26:35 +0800

    test

--- app/Http/Controllers/BrandController.php
+++ app/Http/Controllers/BrandController.php
@@ -65,7 +65,7 @@ class BrandController extends Controller
         $brand->branches = json_decode($brand->branches);
 
         $brand->photo = [
-            'banner'    => $brand->photo['400px'] ?? '',
+            'banner'    => $brand->photo['banner'] ?? '',
             '400px'     => $brand->photo['400px'] ?? '',
             '300px'     => $brand->photo['300px'] ?? '',
             '200px'     => $brand->photo['200px'] ?? '',
