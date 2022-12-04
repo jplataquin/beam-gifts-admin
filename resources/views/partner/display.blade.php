aa7989afd1a249413d83e8f07de4729a36364392
jplataquin <jp.lataquin@gmail.com>
2022-12-04 13:18:54 +0800

    test

--- resources/views/partner/display.blade.php
+++ resources/views/partner/display.blade.php
@@ -24,16 +24,30 @@
     </div>
 
     <div class="row mb-3">
-        <div class="col form-group">
-            <label>Branch Address</label>
-            <textarea id="branch" class="form-control">{{$partner->branch}}</textarea>
-        </div>
-    </div>
-    <div class="row mb-3">
-        <div class="col form-group">
+        <div class="col-md-6 form-group">
             <label>Email</label>
             <input type="email" id="email" class="form-control" value="{{$partner->email}}"/>
         </div>
+
+        <div class="col-md-6 form-group">
+            <label>Status</label>
+            <select id="status" class="form-control">
+                <option value="PEND" @if($partner->status == "PEND") selected="true" @endif >Pending</option>
+                <option value="ACTV" @if($partner->status == "ACTV") selected="true" @endif >Active</option>
+                <option value="SUSP"  @if($partner->status == "SUSP") selected="true" @endif >Suspended</option>
+            </select>
+        </div>
     </div>
+
+    <div class="row mb-3">
+        <div class="col-md-12">
+            <div class="form-group">
+                <label>Branch address</label>
+                <textarea class="form-control" id="">{{$partner->branch}}</textarea>
+            </div>
+        </div>
+    </div>
+
+
 </div>
 @endsection
