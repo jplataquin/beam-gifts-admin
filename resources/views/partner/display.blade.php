61fddf834b9e74f0db8df2916e87a59b45aef2bc
jplataquin <jp.lataquin@gmail.com>
2022-12-04 13:12:42 +0800

    test

--- resources/views/partner/display.blade.php
+++ resources/views/partner/display.blade.php
@@ -4,11 +4,11 @@
 <div class="container">
     <h1>Partner » {{$partner->id}}</h1>
     <hr>
-    <div class="row">
+    <div class="row mb-3">
         <div class="col-md-6">
             <div class="form-group">
                 <label>Name</label>
-                <input type="text" id="name" value="{{$partner->name}}" disabled="true"/>
+                <input type="text" class="form-control" id="name" value="{{$partner->name}}" disabled="true"/>
             </div>
         </div>
         <div class="col-md-6">
@@ -20,6 +20,19 @@
                     @endforeach
                 </select>
             </div>
+        </div>
+    </div>
+
+    <div class="row mb-3">
+        <div class="col form-group">
+            <label>Branch Address</label>
+            <textarea id="branch" class="form-control">{{$partner->branch}}</textarea>
+        </div>
+    </div>
+    <div class="row mb-3">
+        <div class="col form-group">
+            <label>Email</label>
+            <input type="email" id="email" class="form-control" value="{{$partner->email}}"/>
         </div>
     </div>
 </div>
